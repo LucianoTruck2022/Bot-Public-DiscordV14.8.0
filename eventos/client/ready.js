a2c9133d37a27a827860156bd17c11b28c4b0d89
LucianoTruck2022 <122136050+LucianoTruck2022@users.noreply.github.com>
2023-02-01 21:41:19 -0600

    Add files via upload

---
+++ eventos/client/ready.js
@@ -0,0 +1,35 @@
+const { Client } = require('discord.js');
+const { loadcommands } = require('../../funciones/funcionesbot');
+const { mongodb } = require('../../botconfig/config.json');
+const mongoose = require('mongoose');
+require('colors');
+
+module.exports = {
+    name: 'ready',
+    once: true,
+
+    /**
+     * 
+     * @param {Client} client 
+     */
+    async execute(client){
+
+        loadcommands(client).then(() => {
+
+            console.log(` [SISTEMA] :: Conectado como ${client.user.tag}`.blue)
+
+        }).catch((error) => { })
+        //////////////////////////////////////////////////////////////////////////////
+        if(!mongodb) return
+        mongoose.set("strictQuery", false),
+        mongoose.connect(mongodb.uri, {
+
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+
+        }).then(() => {
+
+            console.log(' [MONGODB] :: Base de datos conectado'.blue)
+        }).catch((error) => console.log(error))
+    }
+}
